646a593209f8706773e52e8b1ebef08ffb49d8ca
Deepak Malik <65271369+deepakmalikk@users.noreply.github.com>
2021-05-15 18:37:29 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,197 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var emojiDictionary = {
+  "😀": "Grinning Face",
+  "😃": "Grinning Face with Big Eyes",
+  "😄": "Grinning Face with Smiling Eyes",
+  "😁": "Beaming Face with Smiling Eyes",
+  "😆": "Grinning Squinting Face",
+  "😅": "Grinning Face with Sweat",
+  "🤣": "Rolling on the Floor Laughing",
+  "😂": "Face with Tears of Joy",
+  "🙂": "Slightly Smiling Face",
+  "🙃": "Upside-Down Face",
+  "😉": "Winking Face",
+  "😊": "Smiling Face with Smiling Eyes",
+  "😇": "Smiling Face with Halo",
+  "🥰": "Smiling Face with Hearts",
+  "😍": "Smiling Face with Heart-Eyes",
+  "🤩": "Star-Struck",
+  "😘": "Face Blowing a Kiss",
+  "😗": "Kissing Face",
+  "☺": "Smiling Face",
+  "😚": "Kissing Face with Closed Eyes",
+  "😙": "Kissing Face with Smiling Eyes",
+  "😋": "Face Savoring Food",
+  "😛": "Face with Tongue",
+  "😜": "Winking Face with Tongue",
+  "🤪": "Zany Face",
+  "😝": "Squinting Face with Tongue",
+  "🤑": "Money-Mouth Face",
+  "🤗": "Hugging Face",
+  "🤭": "Face with Hand Over Mouth",
+  "🤫": "Shushing Face",
+  "🤔": "Thinking Face",
+  "🤐": "Zipper-Mouth Face",
+  "🤨": "Face with Raised Eyebrow",
+  "😐": "Neutral Face",
+  "😑": "Expressionless Face",
+  "😶": "Face Without Mouth",
+  "😏": "Smirking Face",
+  "😒": "Unamused Face",
+  "🙄": "Face with Rolling Eyes",
+  "😬": "Grimacing Face",
+  "🤥": "Lying Face",
+  "😌": "Relieved Face",
+  "😔": "Pensive Face",
+  "😪": "Sleepy Face",
+  "🤤": "Drooling Face",
+  "😴": "Sleeping Face",
+  "😷": "Face with Medical Mask",
+  "🤒": "Face with Thermometer",
+  "🤕": "Face with Head-Bandage",
+  "🤢": "Nauseated Face",
+  "🤮": "Face Vomiting",
+  "🤧": "Sneezing Face",
+  "🥵": "Hot Face",
+  "🥶": "Cold Face",
+  "🥴": "Woozy Face",
+  "😵": "Dizzy Face",
+  "🤯": "Exploding Head",
+  "🤠": "Cowboy Hat Face",
+  "🥳": "Partying Face",
+  "😎": "Smiling Face with Sunglasses",
+  "🤓": "Nerd Face",
+  "🧐": "Face with Monocle",
+  "😕": "Confused Face",
+  "😟": "Worried Face",
+  "🙁": "Slightly Frowning Face",
+  "😮": "Face with Open Mouth",
+  "😯": "Hushed Face",
+  "😲": "Astonished Face",
+  "😳": "Flushed Face",
+  "🥺": "Pleading Face",
+  "😦": "Frowning Face with Open Mouth",
+  "😧": "Anguished Face",
+  "😨": "Fearful Face",
+  "😰": "Anxious Face with Sweat",
+  "😥": "Sad but Relieved Face",
+  "😢": "Crying Face",
+  "😭": "Loudly Crying Face",
+  "😱": "Face Screaming in Fear",
+  "😖": "Confounded Face",
+  "😣": "Persevering Face",
+  "😞": "Disappointed Face",
+  "😓": "Downcast Face with Sweat",
+  "😩": "Weary Face",
+  "😫": "Tired Face",
+  "🥱": "Yawning Face",
+  "😤": "Face with Steam From Nose",
+  "😡": "Pouting Face",
+  "😠": "Angry Face",
+  "🤬": "Face with Symbols on Mouth",
+  "😈": "Smiling Face with Horns",
+  "👿": "Angry Face with Horns",
+  "💀": "Skull",
+  "💩": "Pile of Poo",
+  "🤡": "Clown Face",
+  "👹": "Ogre",
+  "👺": "Goblin",
+  "👻": "Ghost",
+  "👽": "Alien",
+  "👾": "Alien Monster",
+  "🤖": "Robot",
+  "😺": "Grinning Cat",
+  "😸": "Grinning Cat with Smiling Eyes",
+  "😹": "Cat with Tears of Joy",
+  "😻": "Smiling Cat with Heart-Eyes",
+  "😼": "Cat with Wry Smile",
+  "😽": "Kissing Cat",
+  "🙀": "Weary Cat",
+  "😿": "Crying Cat",
+  "😾": "Pouting Cat",
+  "💋": "Kiss Mark",
+  "👋": "Waving Hand",
+  "🤚": "Raised Back of Hand",
+  "🖐️": "Hand with Fingers Splayed",
+  "✋": "Raised Hand",
+  "🖖": "Vulcan Salute",
+  "👌": "OK Hand",
+  "🤏": "Pinching Hand",
+  "✌": "Victory Hand",
+  "🤞": "Crossed Fingers",
+  "🤟": "Love-You Gesture",
+  "🤘": "Sign of the Horns",
+  "🤙": "Call Me Hand",
+  "👈": "Backhand Index Pointing Left",
+  "👉": "Backhand Index Pointing Right",
+  "👆": "Backhand Index Pointing Up",
+  "🖕": "Middle Finger",
+  "👇": "Backhand Index Pointing Down",
+  "☝": "Index Pointing Up",
+  "👍": "Thumbs Up",
+  "👎": "Thumbs Down",
+  "✊": "Raised Fist",
+  "👊": "Oncoming Fist",
+  "🤛": "Left-Facing Fist",
+  "🤜": "Right-Facing Fist",
+  "👏": "Clapping Hands",
+  "🙌": "Raising Hands",
+  "👐": "Open Hands",
+  "🤲": "Palms Up Together",
+  "🤝": "Handshake",
+  "🙏": "Folded Hands",
+  "✍": "Writing Hand",
+  "💅": "Nail Polish",
+  "🤳": "Selfie",
+  "💪": "Flexed Biceps",
+  "🦾": "Mechanical Arm",
+  "🦿": "Mechanical Leg",
+  "🦵": "Leg",
+  "🦶": "Foot",
+  "👂": "Ear",
+  "🦻": "Ear with Hearing Aid",
+  "👃": "Nose",
+  "🧠": "Brain",
+  "♥": "love"
+};
+var emojis = Object.keys(emojiDictionary);
+
+export default function App() {
+  var [meaning, setMeaning] = useState("");
+  function onChangeHandler(event) {
+    var input = event.target.value;
+    var meaning = emojiDictionary[input];
+    if (input === undefined) {
+      meaning = "This Emoji is not in our Database";
+    }
+    setMeaning(meaning);
+  }
+  function clickhandler(emojis) {
+    var meaning = emojiDictionary[emojis];
+    setMeaning(meaning);
+  }
+  return (
+    <div className="App">
+      <h1>Emoji interprter</h1>
+      <input
+        placeholder="Enter your emoji here..."
+        onChange={onChangeHandler}
+      ></input>
+      <h2>{meaning}</h2>
+      <h3>Emojis in our Database </h3>
+      {emojis.map((emoji) => {
+        return (
+          <span
+            key={emoji}
+            onClick={() => clickhandler(emoji)}
+            style={{ cursor: "pointer", fontSize: "1rem", padding: "0.3rem" }}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
